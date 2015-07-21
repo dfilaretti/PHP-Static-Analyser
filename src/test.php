c6ba5e8033f542765dd6c51c8c0324c21c1f53d3
Daniele Filaretti <dfilaretti@dyn1212-243.wlan.ic.ac.uk>
2015-07-21 11:33:04 +0100

    added missing ops in concrete domain

--- src/test.php
+++ src/test.php
@@ -1,20 +1,5 @@
 <?php
-	function foo() {
-		echo "foo";
-		return 1;
-	}
-	function bar() {
-		echo "bar";
-		return 2;
-	}
-	function baz() {
-		echo "baz";
-		return 3;
-	}
-	if (2 == 2) 
-		$f = "foo";
-	else
-		$f = "bar";
-	$result = $f(1, 2, 3, 4, 5);
-	echo "hey";
+	$a = 1;
+	$c = $a + ($a = 2);
+	echo $c;
 ?>
