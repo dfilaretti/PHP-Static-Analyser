4db7104224073e6f6681165d562d3667cd463c7a
Daniele Filaretti <dfilaretti@dyn1210-65.wlan.ic.ac.uk>
2015-07-28 17:33:28 +0100

    more examples

--- src/examples/comparison-with-other-tools/crazy-1.php
+++ src/examples/comparison-with-other-tools/crazy-1.php
@@ -19,27 +19,4 @@ function bar($x) {
 
 foo("bar",$_GET["something"]);
 
-
-
-
-/*
-$x[NULL] = 123;
-var_dump($x);
-*/
-/*
-=======
-/*
-class foo { 
-	public $a = "foo";
-	public function bar() { 
-		echo $this->a;
-		$x="this";
-		echo $$x->a;
-	} 
-} 
-
-$y = new foo;
-$y->bar();
-*/
-
 ?>
