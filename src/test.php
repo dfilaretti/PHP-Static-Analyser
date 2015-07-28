869350741948a8e4f3a92d4d0ab3ef583ce912af
raphaelrieuhelft <raphael.rieuhelft@gmail.com>
2015-07-28 15:25:23 +0100

    Kompiles again, add default debugging cases

--- src/test.php
+++ src/test.php
@@ -1,6 +1,18 @@
 <?php
+echo(0 == 0); // 0 == 0 -> true
+echo(0 == "a"); // 0 == 0 -> true
+echo("1" == "01"); // 1 == 1 -> true
+echo("10" == "1e1"); // 10 == 10 -> true
+echo(100 == "1e2"); // 100 == 100 -> true
 
-$x = 1;
+switch ("a") {
+case 0:
+    echo "0";
+    break;
+case "a": // never reached because "a" is already matched with 0
+    echo "a";
+    break;
+}
 
 /*
 $x[NULL] = 123;
